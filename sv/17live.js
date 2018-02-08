bc89c62794360715029e1794ac5d3b98d11ad10d
Israel Sanchez <israel.sanchez80@outlook.com>
2018-02-08 02:12:46 -0600

    archivos c17
    seventeen live

---
+++ sv/17live.js
@@ -0,0 +1,57 @@
+//ver vista previa aun cuando no estén disponibles
+
+var idGirl = 10645016; //tallona
+var hovereffectDiv = $('.overlay inline-preview').closest('.hovereffect');
+hovereffectDiv.find('')
+
+
+//cuando está en rojo
+//obtener div de la chica
+function verChicaOcupada(pId){
+	var maintDiv = $('a[data-id="' + pId + '"]').closest('.overlay-wrapper.model-wrapper');
+	//remover div que dice que está en privado
+	maintDiv.find('.model-tag.model-private').remove();
+	//cambiar class para poder ver preview
+	var overlayDiv = maintDiv.find('.overlay');
+	//agregar class
+	overlayDiv.addClass('overlay inline-preview');
+	
+	var okDiv = maintDiv.find('.inline-preview a:first');
+	var linkChica = okDiv.attr('href');
+	var nombreChica = linkChica.split('/')[2];
+	
+	
+	//ocultar elementos que bloquean pantalla
+	//https://seventeenlive.com/share/friend?audio=0&controls=0&id=10628055
+    var width = 438; //219;
+	var height = 328; //164;
+	var content = '<iframe src=\"/share/friend?audio=0&controls=0&id=' + pId + '\" scrolling=\"no\" id=\"preview-iframe\" name=\"Live Cam Preview\" style=\"position: absolute; top:0px; left: 0px; z-index: 20; pointer-events: none; border: 0px; width: ' + width + 'px; height: ' + height + 'px; opacity:1; \"></iframe>';     
+	//maintDiv.find('.absolute').append(content);
+	
+	var contenedorDiv = $('.col-lg-4.col-md-6.hidden-xs.hidden-sm.hidden-md');
+	contenedorDiv.empty();
+	contenedorDiv.css('position', 'absolute');
+	contenedorDiv.css({top:100, left:100})
+	contenedorDiv.prepend('<div style="border: solid 1px #000000;">' + content + '</div>');
+	
+	okDiv.append('<span>;)</span>');
+			
+	// maintDiv.find
+	maintDiv.find('.inline-loader').addClass('hide');
+	maintDiv.find('.bottom-stick.hidden-xs.hidden-sm').remove();
+	
+	console.log(content);
+	console.log('vamos a ver a ' + nombreChica)
+}
+
+
+verChicaOcupada(10547950); //Joei
+verChicaOcupada(10472547);//sophy
+verChicaOcupada(10507158);//merida
+verChicaOcupada(10472588);//dayana
+verChicaOcupada(10472547);//sophy
+verChicaOcupada(10638925);//honneyCaramel
+verChicaOcupada(10472669);//naomia
+verChicaOcupada(10628055);//yoko
+verChicaOcupada(10616691);//katie
+$('#foo').trigger('mouseover');
